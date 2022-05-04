900d8bec86e9d14b327b0a514c535c0e7cf9ae3e
Douglas Hwang <douglas821209@gmail.com>
2022-05-04 00:25:31 -0700

    general form

--- src/components/lantern/Lantern.tsx
+++ src/components/lantern/Lantern.tsx
@@ -15,6 +15,11 @@ export const Lantern: React.FC = () => {
             <NameLabel>Douglas Hwang</NameLabel><br/>
             <BioLabel>I am a software engineer specializing in building and designing web applications.</BioLabel>
             </Card>
+            {
+            window.screen.width <= 768 ?
+            <p>here is some thing</p> :
+            <p>there is nothing</p>
+            }
             <MySpline scene="https://prod.spline.design/RhniX4xuYOGdSlt8/scene.spline" />
         </Container>
 
